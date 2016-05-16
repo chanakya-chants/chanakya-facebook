8d47af1e112e04997a7b851b04a1ff2d3f8f8f13
Suman Paul <sumank.paul@gmail.com>
2016-05-16 19:39:23 +0530

    get chanakya created app and init the FB bot server

--- index.js
+++ index.js
@@ -1,64 +1,73 @@
-var _ = require('lodash'),
-  express = require('express'),
-  server = express(),
-  bodyParser = require('body-parser'),
-  https = require('https'),
-  path = require('path'),
-  C = require('chanakya');
+(function () {
+  var _ = require('lodash'),
+    express = require('express'),
+    server = express(),
+    bodyParser = require('body-parser'),
+    https = require('https'),
+    path = require('path'),
+    C = require('chanakya');
 
-var chatSession = {};
+  var chatSession = {};
 
-server.set('port', (process.env.PORT || 3000));
+  var fb = {};
 
-server.use(bodyParser.urlencoded({extended: false}));
-server.use(bodyParser.json());
-server.use('/img', express.static(__dirname + '/img'));
+  fb.init = function (app) {
+    server.set('port', (process.env.PORT || 3000));
 
-server.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../public', 'index.html'));
-});
+    server.use(bodyParser.urlencoded({extended: false}));
+    server.use(bodyParser.json());
+    server.use('/img', express.static(__dirname + '/img'));
 
-server.get('/mi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../public', 'mi.png'));
-});
+    server.get('/', function (req, res) {
+      res.sendFile(path.join(__dirname, '../../public', 'index.html'));
+    });
 
-server.get('/webhook', function (req, res) {
-  console.log('get webhook' + req.query['hub.verify_token']);
-  if (req.query['hub.verify_token'] === app.token) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Error, wrong validation token');
-  }
-});
+    server.get('/mi.png', function (req, res) {
+      res.sendFile(path.join(__dirname, '../../public', 'mi.png'));
+    });
 
-server.post('/webhook/', function (req, res) {
+    server.get('/webhook', function (req, res) {
+      console.log('get webhook' + req.query['hub.verify_token']);
+      if (req.query['hub.verify_token'] === app.token) {
+        res.send(req.query['hub.challenge']);
+      } else {
+        res.send('Error, wrong validation token');
+      }
+    });
 
-  messaging_events = req.body.entry[0].messaging;
+    server.post('/webhook/', function (req, res) {
 
-  for (i = 0; i < messaging_events.length; i++) {
-    var event = req.body.entry[0].messaging[i];
-    var sender = event.sender.id;
+      messaging_events = req.body.entry[0].messaging;
 
-    if (_.isUndefined(chatSession[sender])) {
-      https.get('https://graph.facebook.com/v2.6/' + sender + '?access_token=' + app.token, function (res) {
-        res.setEncoding('utf8');
-        res.on('data', function (d) {
-          d = JSON.parse(d);
-          d.id = sender;
-          d.expectation = app.expectation;
-          chatSession[sender] = _.clone(d);
+      for (i = 0; i < messaging_events.length; i++) {
+        var event = req.body.entry[0].messaging[i];
+        var sender = event.sender.id;
+
+        if (_.isUndefined(chatSession[sender])) {
+          https.get('https://graph.facebook.com/v2.6/' + sender + '?access_token=' + app.token, function (res) {
+            res.setEncoding('utf8');
+            res.on('data', function (d) {
+              d = JSON.parse(d);
+              d.id = sender;
+              d.expectation = app.expectation;
+              chatSession[sender] = _.clone(d);
+              C.handleMessage(event, chatSession[sender]);
+            });
+          }).on('error', function (e) {
+            console.error(e);
+          });
+        } else {
           C.handleMessage(event, chatSession[sender]);
-        });
-      }).on('error', function (e) {
-        console.error(e);
-      });
-    } else {
-      C.handleMessage(event, chatSession[sender]);
-    }
-  }
-  res.sendStatus(200);
-});
+        }
+      }
+      res.sendStatus(200);
+    });
 
-server.listen(server.get('port'), function () {
-  console.log('Node server is running on port', server.get('port'));
-});
+    server.listen(server.get('port'), function () {
+      console.log('Node server is running on port', server.get('port'));
+    });
+  };
+
+  module.exports = fb;
+
+}());
